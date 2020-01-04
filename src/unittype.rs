78da161dd00f8c8b1c8e3e9ccb934e9d4f176369
danielvschoor <43602193+danielvschoor@users.noreply.github.com>
2020-01-04 23:11:23 +0200

    Fix armor type (again)

--- src/unittype.rs
+++ src/unittype.rs
@@ -28,7 +28,7 @@ pub struct UnitType {
     /// Max shield
     pub max_shield: Option<u32>,
     /// Armor
-    pub armor: f32,
+    pub armor: R32,
     /// Vision range
     pub sight: R32,
     /// None if not detector
